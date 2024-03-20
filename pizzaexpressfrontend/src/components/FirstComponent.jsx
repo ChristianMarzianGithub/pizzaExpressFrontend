b1882efacf3ac457d1e4adf751535c8ab0b9a12e
cmarzian91@gmail.com <cmarzian91@gmail.com>
2024-03-21 00:37:18 +0100

    added shopping basket

--- pizzaexpressfrontend/src/components/FirstComponent.jsx
+++ pizzaexpressfrontend/src/components/FirstComponent.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import DataTable from './DataTable';
 import axios from "axios";
+import pizzaImage from '../images/pizza_salami.jpg';
 
 function retrieveAllPizzas(setPizzas){
     axios.get('http://localhost:8080/pizza/getAllPizzas')
@@ -10,7 +11,6 @@ function retrieveAllPizzas(setPizzas){
 }
 
 function successfullResponse(data,setPizzas){
-    console.log(data);
     setPizzas(data);
 }
 
@@ -21,20 +21,70 @@ function errorResponse(error){
 const FirstComponent = () => {
     const [pizzas, setPizzas] = useState([]);
 
-  const data = [
-    { id: 1, name: 'John Doe', email: 'john@example.com' },
-    { id: 2, name: 'Jane Smith', email: 'jane@example.com' },
-    // Add more data as needed
-  ];
-
   useEffect(() => {
     retrieveAllPizzas(setPizzas);
   }, []);
 
+  // Define state to manage the basket items
+ const [basket, setBasket] = useState([]);
+
+ // Function to add an item to the basket
+ const addItemToBasket = (item) => {
+  setBasket(prevBasket => [...prevBasket, item]);
+  console.log(basket);
+ };
+
   return (
     <div>
         <h1>My Data Table</h1>
-        <DataTable data={pizzas} />
+        <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Name</th>
+          <th>Ingredients</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        {pizzas.map(item => (
+          <tr key={item.id}>
+            <td>{item.id}</td>
+            <td>{item.name}</td>                        
+            <td>
+              <ul>
+                {item.ingredients.map((ingredient, index) => (
+                  <li key={index}>{ingredient.name},{ingredient.quantity} {ingredient.unit}</li>                  
+                ))}
+              </ul>
+            </td>
+            <td><img src={pizzaImage} alt="Pizza" id="small-image"/></td>
+            <td>
+                {/* Button to add item to basket */}
+                <button onClick={() => addItemToBasket(item)}>Add to Basket</button>
+              </td>              
+          </tr>
+        ))}
+      </tbody>
+    </table>
+    <table>
+      <thead>
+        <tr>
+          <th>Warenkorb</th>
+        </tr>
+      </thead>
+      <tbody>
+        {basket.map(item => (
+              <tr key={item.id}>
+                <td>{item.id}</td>
+                <td>{item.name}</td>
+              </tr>
+            )
+        )}
+        
+      </tbody>            
+    </table>
         
     </div>
   );
